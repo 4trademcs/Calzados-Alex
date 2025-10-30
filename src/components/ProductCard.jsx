7ea49613f898af6fc2e5d738dcf7210e79a95042
4trademcs <4trademcs@gmail.com>
2025-10-30 19:15:37 -0400

    2models

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -22,7 +22,7 @@ const SANDALIAS_BLOCKED = [];
 
 /* Modelos no disponibles (se muestran en B/N) */
 const UNAVAILABLE = [33];
-const NEW =[2];
+const NEW =[2,23,30];
 
 /* ===================== Helpers ===================== */
 function getModelNumber(name, id) {
